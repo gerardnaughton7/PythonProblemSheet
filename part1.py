186087abca439d828ece18f64dfa2a3d5f0014ee
gerard naughton <gerard naughton>
2017-09-23 13:06:50 +0100

    Part 1 Completed HelloWorld

---
+++ part1.py
@@ -0,0 +1,7 @@
+# Python Problem Sheet 
+# Gerard Naughton G00209309
+# Part 1
+# Objective: Write a program that prints “Hello, world!” to the screen.
+
+# Print "Hello, World" to console
+print("Hello, World")
