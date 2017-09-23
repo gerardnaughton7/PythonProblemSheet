4472998d6c3c8f640b01c477d323f4b4f758def4
gerard naughton <gerard naughton>
2017-09-23 13:45:38 +0100

    Part 2 Complete Date and Time

---
+++ part2.py
@@ -0,0 +1,10 @@
+# Python Problem Sheet 
+# Gerard Naughton G00209309
+# Part 2
+# Objective: Write a program that prints the current time and date to the console.
+
+# Import Time 
+import time
+
+# Print Current Date and Time to console
+print (time.strftime("Todays date %d/%m/%Y and time is %H:%M:%S"))
