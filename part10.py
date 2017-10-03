6ef52a3e344e355c3de5160ff9fe8758afb77c07
gerard naughton <gerard naughton>
2017-10-03 12:03:43 +0100

    Part 10 complete Reverse String

--- part10.py
+++ part10.py
@@ -7,6 +7,21 @@
 def str_reverse(str1):
     return str1[::-1]
 
-str1 = input("Please enter a word? ").lower()
+# function 2 puts the string into a list. then using the pop function on the before List we remove and return the last element of the list and append it too the after List. we loop through the list until all elements have been appended. Adapted from : https://www.tutorialspoint.com/python/list_pop.htm
+def str_reverse2(str2):
+    before = list(str2)
+    after = list()
+    while len(before) > 0:
+        after.append(before.pop())
 
+    return ''.join(after)
+
+# Input string 1
+str1 = input("Please enter a word? ").lower()
+# print out result using str_reverse function
 print(str_reverse(str1))
+
+# input string 2
+str2 = input("Please enter a word? ").lower()
+# print out result using ste_reverse2
+print(str_reverse2(str2))
